3c4aa6ac700132cac68430f4e013d044dac73abc
Chris Borrill <chris.borrill@gmail.com>
2019-03-02 18:46:09 +1300

    Reduce size of font program, update refresh freq to 5s

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 """Main program of GPS speedometer."""
 import screen
 import machine
-import utime
 
 s = screen.Screen()
 s.update(6.5)
 
-utime.sleep_ms(20000)
-machine.deepsleep(20000)
+machine.deepsleep(5000)
